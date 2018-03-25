112cf6948306d6d774ef3ebc9214a6f615049a3b
Molly Moran <mollyclaremoran@gmail.com>
2018-03-25 16:05:03 -0500

    basic sign up form

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,13 +1,68 @@
-import React, {Component} from "react";
-import { Grid, Row, Col } from 'react-bootstrap';
-import FontAwesome from 'react-fontawesome';
+import React, { Component } from "react";
+import { Row, Col, FormGroup, ControlLabel, FormControl, HelpBlock, InputGroup, Button } from "react-bootstrap";
+import FontAwesome from "react-fontawesome";
 import "./Signup.css";
 
+class Signup extends React.Component {
+    state = {
+        email: '',
+        password: '',
+    };
 
-const Signup = props => (
-    <Row>
-        <Col xs={12}>This is the sign up page.</Col>
-    </Row>
-);
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+        console.log("state: ", this.state);
+    };
+    
+    // handleFormSubmit = event => {
+    //     event.preventDefault();
+    //     console.log("form submitted", this.state);
+            
+    //     API.saveCard({
+    //         name: this.state.title,
+    //         amount: this.state.amount,
+    //         category: this.state.category
+    //         })
+    //         .then(res => console.log(res))
+    //         .catch(err => console.log(err));
+    // };
+    
+    render() {
+        return (
+            <Row>
+                <Col xs={12}>
+                    <form>
+                        <h3>Create an Account</h3>
+                        {/* <FormGroup controlId="formBasicText" validationState={this.getValidationState()}> */}
+                        <FormGroup>
+                            <ControlLabel>Enter your email address. This will be your username.</ControlLabel>
+                            <FormControl 
+                                type="text" 
+                                value={this.state.email} 
+                                name="email" 
+                                placeholder="YourName@YourEmail.com" 
+                                onChange={this.handleInputChange} />
+                        </FormGroup>
+
+                        <FormGroup>
+                            <ControlLabel>Enter a password with at least 5 characters.</ControlLabel>
+                                <FormControl 
+                                    type="password" 
+                                    value={this.state.password} 
+                                    name="password" 
+                                    placeholder="password" 
+                                    onChange={this.handleInputChange} />
+                        </FormGroup>
+
+                        <Button type="submit" block>Submit</Button>
+                    </form>
+                </Col>
+            </Row>
+        );
+    }
+}
 
 export default Signup;
