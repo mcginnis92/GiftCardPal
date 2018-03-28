4ebfb2d3f170cddc3ebc6906dc0e8b858cbdea44
Molly Moran <mollyclaremoran@gmail.com>
2018-03-28 12:15:15 -0500

    adds amount spent form field

--- client/src/components/GiftCard/GiftCardModal/GiftCardModal.js
+++ client/src/components/GiftCard/GiftCardModal/GiftCardModal.js
@@ -1,27 +1,59 @@
 import React from "react";
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, FormGroup, FormControl, InputGroup, ControlLabel } from 'react-bootstrap';
 import "./GiftCardModal.css";
 
-const GiftCardModal = props => (
-    <div>
-        <Modal.Dialog>
-            <Modal.Header>
-                <h3>{props.name}</h3>
-            </Modal.Header>
+class GiftCardModal extends React.Component {
 
-            <Modal.Body>
-                <p>Gift Card Number: {props.number}</p>
-                <p>PIN: {props.pin}</p>
-            </Modal.Body>
+    state = {
+        toggle: this.props.toggle,
+        name: this.props.name,
+        number: this.props.number,
+        pin: this.props.pin,
+        amount: ''
+    };
 
-            <Modal.Footer>
-                <Button onClick={props.toggle}>Close</Button>
-                <Button bsStyle="primary">Save changes</Button>
-            </Modal.Footer>
-    
-        </Modal.Dialog>
-    </div>
-);
+    handleInputChange = event => {
+        this.setState({
+            amount: event.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Modal.Dialog>
+                    <Modal.Header closeButton onClick={this.state.toggle}>
+                        <Modal.Title><strong>{this.state.name}</strong></Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p>Gift Card Number: {this.state.number}</p>
+                        <p>PIN: {this.state.pin}</p>
+
+                        <FormGroup>
+                            <ControlLabel>Update Amount Spent</ControlLabel>
+                            <InputGroup>
+                                <InputGroup.Addon>$</InputGroup.Addon>
+                                <FormControl 
+                                    type="text" 
+                                    value={this.state.amount}
+                                    name="amount" 
+                                    // placeholder="" 
+                                    onChange={this.handleInputChange} />
+                            </InputGroup>
+                        </FormGroup>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button onClick={this.state.toggle}>Close</Button>
+                        <Button bsStyle="primary">Save changes</Button>
+                    </Modal.Footer>
+            
+                </Modal.Dialog>
+            </div>
+        )
+    }    
+};
 
 
 export default GiftCardModal;
