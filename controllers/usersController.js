e12533b9ba877125517d499e42bf1f91bceccf95
Molly Moran <mollyclaremoran@gmail.com>
2018-04-02 10:35:02 -0500

    correctly validates login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -33,81 +33,23 @@ module.exports = {
     });
   },
 
-  //LOGIN A USER
-  // login: function(req, res){
-  //   console.log("you hit the login user route");
-  //   console.log("req.body", req.body)
-
-  //   // var myPlaintextPassword = req.body.password;
-    
-  //   // bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
-  //     db.User.findOne({
-  //       username: req.body.username,
-  //       password: req.body.password
-  //     })
-    
-  //       .then(userData => res.json(userData))
-  //       .catch(err => res.status(422).json(err))
-  //       // .catch(err => console.log(err.response));
-  //   // })
-  //   // })
-  // },
-  
   login: function(req, res){
+    // fetch user and test password verification
+    db.User.findOne({ username: req.body.username }, function(err, user) {
+      if (err) throw err;
 
-    var myPlaintextPassword = req.body.password;
-    var someOtherPlaintextPassword = 'hello';
+    // test a matching password
+    user.comparePassword(req.body.password, function(err, isMatch) {
+        if (err) throw err;
+        console.log(req.body.password, isMatch); 
+    });
 
-    bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
-      console.log("hashed password", hash)
-  
-        // Load hash from your password DB.
-        bcrypt.compare(myPlaintextPassword, hash).then(function(res) {
-          db.User.findOne({
-            username: req.body.username,
-            password: hash
-          })
-            console.log("res is", res)
-              res === true ? console.log('true') : console.log('false')
-        })
-        //checking a fake password
-        // bcrypt.compare(someOtherPlaintextPassword, hash).then(function(res) {
-        //   db.User.findOne({
-        //     username: req.body.username,
-        //     password: someOtherPlaintextPassword
-        //   })
-        //     .then(dbModel => res.json(dbModel))
-        //     .catch(err => res.json(err))
-        // });
-    })
-    .then(userData => res.send(userData))
-    .catch(err => res.status(422).json(err));
-  }
+    // test a failing password
+    user.comparePassword('123Password', function(err, isMatch) {
+        if (err) throw err;
+        console.log('123Password:', isMatch); 
+    });
+  });
+}
 
-
-
-
-
-  // login: function(req, res) {
-  //   var username = req.body.username;
-  //   var password = req.body.password;
-
-  //   passport.use(
-  //     new LocalStrategy(function(username, password, done) {
-  //       User.findOne({ username: username }, function(err, user) {
-  //         if (err) {
-  //           return done(err);
-  //         }
-  //         if (!user) {
-  //           return done(null, false, { message: "Incorrect username." });
-  //         }
-
-  //         if (!user.validPassword(password)) {
-  //           return done(null, false, { message: "Incorrect password." });
-  //         }
-  //         return done(null, false, { message: "Incorrect password." });
-  //       });
-  //     })
-  //   );
-  // }
 };
